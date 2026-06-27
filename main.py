0f2484f0fdf60c7a2d9f58c9eb69f951f8d3b74f
dev-mir2011 <ruhaanmir.w@gmail.com>
2026-06-27 09:28:41 +0530

    Bug Fix

--- main.py
+++ main.py
@@ -3,28 +3,26 @@ import secrets
 
 CHARS = string.ascii_letters + string.punctuation + string.digits
 
+
 def main():
     command_str = input("> ")
     command = command_str.split(" ")
     if command[0].lower() == "exit" or command[0].lower() == "q":
         quit()
     elif command[0].lower() == "gen":
-        length = command[1]
-        if length is not None:
-            if length != "" or length != " ":
-                print("Password Length Must Be Greater Than 0") 
-            else:
-                if int(length) > 0:
-                    password = "".join(secrets.choice(CHARS) for _ in range(int(length)))
-                    print(f"PASSWORD: {password}")
-                else:
-                    print("Password Length Must Be Greater Than 0") 
+        length = 0
+        try:
+            length = int(command[1])
+        except ValueError:
+            print("Please Enter a Valid Password Length")
+        if length > 0:
+            password = "".join(secrets.choice(CHARS) for _ in range(int(length)))
+            print(f"PASSWORD: {password}")
         else:
-            print("Password Length Must Be provided")
-        
-        
-        
-        
-    
+            print("Password Length Must Be Greater Than 0")
+    else:
+        print("Unknown Command, Valid Commands Are\n gen [length (int)]\n quit")
+
+
 while True:
     main()
